4def5608e79987e98ac7b831cc67da850d7053d0
Tanguy Coatalem <tanguy.coatalem@gmail.com>
2018-02-01 00:08:56 +0100

    Added default database to ensure we can initialize faster

--- DD35/static/DD35/index.js
+++ DD35/static/DD35/index.js
@@ -1,20 +1,24 @@
-function PostMessage(e) {
-    if (e.keyCode == 13) {
-        var message = $('#btn-input').val();
-        if (message){
-            $.post({
-                url: 'ajax/chat/',
-                data: {
+function PostMessage(chatId) {
+    var btnInput = $('#btn-input-'.concat(chatId))
+    var message = btnInput.val();
+    if (message){
+        $.post({
+            url: 'ajax/chat/',
+            data: {
                 'message': message,
                 'user': document.getElementById("user").value,
                 'channel': document.getElementById("currentChannel").value
-                },
-                success: function (data) {
-                    $('#btn-input').val('');
-                }
-            });
-        }
-        return false;
+            },
+            success: function (data) {
+                btnInput.val('');
+            }
+        });
+    }
+}
+
+function PostMessageIfEnter(e, chatId) {
+    if (e.keyCode == 13) {
+        PostMessage(chatId)
     }
 }
 
@@ -26,7 +30,7 @@ function ChangeChat(chatId, chatName) {
     }
     $('.chatroom').addClass('invisible')
     $('#chatroom-'.concat(chatId)).removeClass('invisible')
-    $('currentChannel').value = chatId.toString()
+    $('#currentChannel').val(chatId.toString())
     selectedChat.removeClass('invisible')
 }
 
@@ -34,39 +38,18 @@ function InitializePusher(chatId, chatName) {
     var pusher = new Pusher('e35aa2ec9d8ab3ac21ce', {cluster: 'eu', encrypted: true});
     var my_channel = pusher.subscribe(chatId.toString());
     my_channel.bind("my-event", function (data) {
-        var new_message = `<li class="left clearfix"><span class="chat-img pull-left">
-                            <img src="http://placehold.it/50/55C1E7/fff&text=`+data.name+`" alt="User Avatar" class="img-circle">
-                        </span>
-                            <div class="chat-body clearfix">
-                                </br>
-                                 <p style="margin-left: 100px">`+data.message+`
-                                </p>
-                            </div>
-                        </li>`;
+        console.log("a")
+            var new_message = `
+                <li class="left clearfix"><span class="chat-img pull-left">
+                    <img src="http://placehold.it/50/55C1E7/fff&text=`+data.name+`" alt="User Avatar" class="img-circle">
+                </span>
+                    <div class="chat-body clearfix">
+                        </br>
+                         <p style="margin-left: 100px">`+data.message+`
+                        </p>
+                    </div>
+                </li>
+            `;
         $('#chatroom-'.concat(chatId).concat('-chat')).append(new_message);
     });
 }
-
-//wait until the DOM is fully ready
-$(document).ready(function(){
-//add event listener to the chat button click
-    $("#btn-chat").click(function(){
-//get the currently typed message
-        var message = $('#btn-input').val();
-        if (message){
-            $.post({
-                url: 'ajax/chat/',
-                data: {
-                'message': message,
-                'user': document.getElementById("user").value,
-                'channel': document.getElementById("currentChannel").value
-                },
-                success: function (data) {
-
-                    $('#btn-input').val('');
-
-                }
-            });
-        }
-    })
-})
